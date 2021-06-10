e7883d24a3b99aa522065a7ad6ed88d221d2ca31
Matt Pawelski <mxpawels@gmail.com>
2021-06-10 01:48:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,16 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  #horizontal wins
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  #vertical wins
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  #diagonal wins
+  [0,4,8],
+  [2,4,6]
+]
